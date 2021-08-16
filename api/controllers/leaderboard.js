97065a79feabe975c0e2a8d56590110cc891c42d
Shav <shavhugan@gmail.com>
2021-08-16 11:44:06 +0100

    controllers created for leadervoard, id route, get route and post route

---
+++ api/controllers/leaderboard.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+const Leaderboard = require('../models/leaderboard')
+
+// leaderboard index routes
+router.get('/', async (req, res) => {
+    try {
+        const leaderboard = await Leaderboard.all
+        res.json({leaderboard})
+    } catch(err) {
+        res.status(500).json({err})
+    }
+})
+
+// Leaderboard show route
+router.get('/:id', async (req, res) => {
+    try {
+        const leaderboard = await Leaderboard.findById(parseInt(req.params.id))
+        res.json(leaderboard)
+    } catch(err) {
+        res.status(404).json({err})
+    }
+})
+
+// Create Leaderboard route
+router.post('/', async (req, res) => {
+    try {
+        const leaderboard = await Leaderboard.create(req.body.name, req.body.score)
+        res.json(leaderboard)
+    } catch(err) {
+        res.status(404).json({err})
+    }
+})
+
+module.exports = router;
