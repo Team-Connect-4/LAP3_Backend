4c197a2f62cca0bf3797422f29543046d31123bd
Shav <shavhugan@gmail.com>
2021-08-18 19:58:03 +0100

    edited sql for get all route

--- api/models/leaderboard.js
+++ api/models/leaderboard.js
@@ -1,7 +1,7 @@
-const db = require ('../dbConfig')
+const db = require('../dbConfig')
 
 class Leaderboard {
-    constructor(data){
+    constructor(data) {
         this.id = data.id
         this.name = data.name
         this.score = data.score
@@ -9,9 +9,9 @@ class Leaderboard {
     }
 
     static get all() {
-        return new Promise (async (resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             try {
-                const leaderboardData = await db.query(`SELECT * FROM leaderboard;`)
+                const leaderboardData = await db.query(`SELECT DISTINCT * FROM leaderboard;`)
                 const leaderboard = leaderboardData.rows.map(d => new Leaderboard(d))
                 resolve(leaderboard);
             } catch (err) {
@@ -20,12 +20,12 @@ class Leaderboard {
         })
     }
 
-    static create(name, score, difficulty){
-        return new Promise (async (resolve, reject) => {
+    static create(name, score, difficulty) {
+        return new Promise(async (resolve, reject) => {
             try {
-                let leaderboardData = await db.query(`INSERT INTO leaderboard (name, score, difficulty) VALUES ($1, $2, $3) RETURNING *;`, [ name, score, difficulty ]);
+                let leaderboardData = await db.query(`INSERT INTO leaderboard (name, score, difficulty) VALUES ($1, $2, $3) RETURNING *;`, [name, score, difficulty]);
                 let newLeaderboard = new Leaderboard(leaderboardData.rows[0]);
-                resolve (newLeaderboard);
+                resolve(newLeaderboard);
             } catch (err) {
                 reject('Error creating Leaderboard Entry');
             }
