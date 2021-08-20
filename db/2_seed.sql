3f1314058a260ff673699669a6f9b624b3bb68be
Shav <shavhugan@gmail.com>
2021-08-20 09:32:01 +0100

    added more seed data for deploy

--- db/2_seed.sql
+++ db/2_seed.sql
@@ -3,4 +3,10 @@ VALUES
     ('Shav', 5, 'medium'),
     ('Humza', 6, 'hard'),
     ('Polina', 5, 'easy'),
-    ('Sammie', 7, 'hard')
+    ('Sammie', 7, 'hard'), 
+    ('Beth', 9, 'hard'),
+    ('Romeo', 1, 'hard'),
+    ('Dialga', 10, 'hard'),
+    ('Palkia', 2, 'medium'),
+    ('Giritina', 5, 'easy'),
+    ('Zelda', 5, 'medium')
