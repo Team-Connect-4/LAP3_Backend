7ac08294b73783bb970d6ed861b2f6a3acf0c880
Shav <shavhugan@gmail.com>
2021-08-20 09:39:50 +0100

    changed port to 2096

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const app = require("./server");
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 2096;
 
 app.listen(port, () => console.log(`Express departed from port ${port}`))
